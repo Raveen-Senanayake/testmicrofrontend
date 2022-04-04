44d678a6eca5aa17546dbc95600aebe7f961f327
Raveen_Senanayake <raveenseshan@live.com>
2022-04-04 16:54:24 +1000

    add marketing deployment 2

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/markerting/latest/'
+        publicPath: '/marketing/latest/'
     },
 
     plugins: [
